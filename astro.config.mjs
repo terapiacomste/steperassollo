f7bd8b4769f25549dfae15d3b3eaf856b8118582
terapiacomste <psiperassollo@gmail.com>
2025-05-07 21:44:17 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://terapiacomste.github.io',
-  base: '/terapiacomste',
+  base: '/steperassollo',
   integrations: [tailwind()],
   outDir: 'dist'
 });
